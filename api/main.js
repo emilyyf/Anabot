dca8506f515a40a54a0d078a9a8f2727e31eb6ca
Emily <68402015+emilyyf@users.noreply.github.com>
2021-09-15 19:35:11 -0300

    feat: Add roll command

--- api/main.js
+++ api/main.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 const api_url = 'https://api.telegram.org/bot' + process.env.BOT_TOKEN;
 
-const send_message = async (id, data, id_reply) => {
+const send_message = async (id, data, id_reply, do_not_parse) => {
 	return await fetch(api_url + '/sendMessage', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
@@ -10,7 +10,7 @@ const send_message = async (id, data, id_reply) => {
 			chat_id: id,
 			text: data,
 			reply_to_message_id: id_reply,
-			parse_mode: 'Markdown'
+			parse_mode: do_not_parse ? undefined : 'Markdown'
 		})
 	})
 		.then(res => console.log(res.json()));
@@ -34,6 +34,25 @@ const sed = async (chat, text, reply, reply_to_message) => {
 	await send_message(parseInt(chat.id), answer, parseInt(reply_to));
 }
 
+const roll = async (chat, text, message_id, username) => {
+	const toRoll = text.split(' ')[1];
+	if (!toRoll) return;
+	const reg = /(\d+)d(\d+)([\+-]\d+)?/;
+	const res = toRoll.match(reg);
+	if (!res) return;
+	if (res.length < 3) return;
+	var answer = username + ' rolls [';
+	var sum = 0;
+	for (i = 0; i < res[1]; ++i) {
+		const buf = Math.floor(Math.random() * res[2] + 1);
+		answer += ' ' + buf + ',';
+		sum+=buf;
+	}
+	answer = answer.replace(/,$/, ' ');
+	answer += '] = ' + sum;
+	await send_message(parseInt(chat.id), answer, parseInt(message_id), true);
+}
+
 module.exports = async (req, res) => {
 	if (!req.body) {
 		res.status(200).send('Ok');
@@ -42,15 +61,14 @@ module.exports = async (req, res) => {
 
 	const { message } = req.body; 
 
-	console.log(message);
 	if (!message) {
-		console.log(req.body);
 		res.status(200).send('Ok');
 		return;
 	}
 
-	const { chat, text } = message;
+	const { chat, text, from } = message;
 	const { reply_to_message, message_id } = message;
+	const { username } = from;
 	const reply = (reply_to_message) ? reply_to_message.from : undefined;
 	if (reply) reply.text = reply_to_message.text;
 	if (reply) reply.date = reply_to_message.date;
@@ -73,6 +91,10 @@ module.exports = async (req, res) => {
 
 	if (text.startsWith('s/') && reply_to_message) {
 		await sed(chat, text, reply, reply_to_message);
+	}
+
+	if (text.startsWith('/roll')) {
+		await roll(chat, text, message_id, username);
 	}
 
 	res.status(200).send('Ok');
